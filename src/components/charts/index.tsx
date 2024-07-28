0b6f061f0e8a2a969b1a9770a5714ff06071550a
Peter Bengtsson <mail@peterbe.com>
2024-07-27 22:26:38 -0400

    ability to minimize charts (#24)

--- src/components/charts/index.tsx
+++ src/components/charts/index.tsx
@@ -2,17 +2,22 @@ import { useDocumentTitle } from "@mantine/hooks"
 
 import { SignedIn } from "../signed-in"
 import { LyricsFeatureflag } from "./lyrics-featureflag"
+import { MinimizeContext } from "./minimize-context"
 import { PageviewEvents } from "./pageview-events"
 import { Pageviews } from "./pageviews"
+import { useMinimized } from "./use-minimized"
 
 export default function Charts() {
   useDocumentTitle("Charts")
 
+  const [minimized, toggleMinimized] = useMinimized()
   return (
     <SignedIn>
-      <Pageviews />
-      <LyricsFeatureflag />
-      <PageviewEvents />
+      <MinimizeContext.Provider value={{ minimized, toggleMinimized }}>
+        <Pageviews />
+        <LyricsFeatureflag />
+        <PageviewEvents />
+      </MinimizeContext.Provider>
     </SignedIn>
   )
 }
